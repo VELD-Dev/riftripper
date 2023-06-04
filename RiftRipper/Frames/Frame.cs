19e6e23fc2d9cbb878551e0c4842adeed8cd4f10
V E L Δ <36549174+VELD-Dev@users.noreply.github.com>
2023-06-04 03:59:48 +0200

    cleaned Frame.cs

--- RiftRipper/Frames/Frame.cs
+++ RiftRipper/Frames/Frame.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RiftRipper.Frames;
+﻿namespace RiftRipper.Frames;
 
 /// <summary>
 /// Frame (window) inside the viewport
